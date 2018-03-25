f5afcc4b90e3d25134190c98a89b9f98dd30a6cd
philFernandez <norcal618@live.com>
2018-03-25 16:17:07 -0700

    both sliders installed

--- public/js/app.js
+++ public/js/app.js
@@ -11,16 +11,42 @@
  */
 
 
+//$('.timer').timer({
+//format: '%H:%M:%S',
+//countdown: true,
+//duration: ui.value + 'm0s',
+//callback: function() {
+//alert('Time Up');
+//}
+//});
 
-//countdown timer
-$('#timerDiv').timer({
-  format: '%H:%M:%S',
-  countdown: true,
-  duration: '0m10s',
-  callback: function() {
-    alert('Time Up');
-  }
+
+
+// worktime slider ======================================
+$(".worktime-slider").slider({
+  min: 1,
+  max: 25,
+  step: 1,
+  orientation: 'vertical'
 });
 
+$(".worktime-slider").on('slide', function(e, ui) {
+  $(".worktime-readout")[0].innerHTML = ui.value + ' min';
+  //console.log($('.worktime'));
+});
+// ======================================================
 
 
+// breaktime slider =====================================
+$('.breaktime-slider').slider({
+  min: 1,
+  max: 25,
+  step: 1,
+  orientation: 'vertical'
+});
+
+$(".breaktime-slider").on('slide', function(e, ui) {
+  $(".breaktime-readout")[0].innerHTML = ui.value + ' min';
+
+});
+// ======================================================
